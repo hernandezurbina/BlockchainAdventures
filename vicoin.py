3969b4ceb0d1174d98efd162230d1538c0ad06d8
hernandezurbina <hernandezurbina@gmail.com>
2020-02-13 10:33:44 +0000

    Working on VICOin script

--- vicoin.py
+++ vicoin.py
@@ -66,9 +66,34 @@ class Blockchain:
         previous_block = self.get_previous_block()
         return previous_block['index'] + 1
 
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
 ### PART 2: Mining our blockchain
 ## create web app
 app = Flask(__name__)
+
+## creating an address for the node on port 5000
+node_address = str(uuid4().replace('-',''))
+
 
 ## create blockchain
 blockchain = Blockchain()
@@ -80,12 +105,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver='Vico', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transactions':block['transactions']}
     return jsonify(response), 200
 
 ## getting the full Blockchain
@@ -96,12 +123,42 @@ def get_chain():
     return jsonify(response), 200
 
 ## checking chain validity
-@app.route('/check_chain', methods=['GET'])
-def check_chain():
-    response = {'is_chain_valid': blockchain.is_chain_valid(blockchain.chain)}
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good, the Blockchain is valid!'}
+    else:
+        response = {'message': 'Houston we have a problem. The Blockchain is NOT valid!'}
     return jsonify(response), 200
 
+## adding transaction to the blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to block {index}'}
+    return jsonify(response), 201
+
 ### PART 3: Decentralizing Blockchain
+# connecting new nodes
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return 'No node', 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected',
+               'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
+# replacing the chain by the longest chain if neeeded
+
 
 ## running app
 app.run(host='0.0.0.0', port=5000)
